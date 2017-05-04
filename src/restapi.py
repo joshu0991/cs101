ac17561bd12a421e7be48eff418a0bc8138a5bd5
behrad <behmardibehrad@gmail.com>
2017-05-03 21:46:23 -0400

    fixed the time issue

--- src/restapi.py
+++ src/restapi.py
@@ -13,7 +13,7 @@ def restful_request():
     mytime = datetime.datetime.now()
     print(mytime)
 
-    now = datetime.datetime.now()
+    now = datetime.datetime.now() - datetime.timedelta(minutes=240)
     now.strftime("%I:%M%p on %B %d, %Y")
 
     url = 'http://54.191.52.234:7272/api/v0.1/eaaefbeed081427b8fd74561f396bd41/push'
@@ -32,7 +32,7 @@ def restful_request():
                                        key='id_20',
                                        data=json.dumps({"title": "Mail Notification",
                                                         "description": "number of New Mails",
-                                                        "big-value": str(counter),
+                                                        "big-value": "0",
                                                         "upper-left-label": "critical:",
                                                         "upper-left-value": "1020",
                                                         "lower-left-label": "major:",
@@ -60,5 +60,5 @@ def restful_request():
     print(r.status_code, r.reason)
     print(r.text)
     print(r.status_code, r.reason)
-#restful_request()
+restful_request()
 
