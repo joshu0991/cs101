00523a5f4db2f33b9f981d7607f0d6b07b2ff903
behrad <behmardibehrad@gmail.com>
2017-05-03 22:37:17 -0400

    counter

--- src/cleantipboard.py
+++ src/cleantipboard.py
@@ -18,37 +18,30 @@ def restful_request():
 
     url = 'http://54.191.52.234:7272/api/v0.1/eaaefbeed081427b8fd74561f396bd41/push'
     data_push_statistic = dict(tile='listing', key='id_13', data=json.dumps({"title": "Statistics",
-    "items": [[" "],["  Mail Recieved on "],["Date : " + now.strftime("%B %d, %Y")],
-              ["Time : " + now.strftime("%I:%M%p")],
+    "items": [[" "],["  Mail Recieved on "],["Date : " ],
+              ["Time : " ],
               [""]]}))
 
     data_push_group_memebrs = dict(tile='listing',
                      key='id_1',
                      data=json.dumps({
                          "title": "Group Memebers",
-                         "items": [["\t\tCS 101 GROUP PROJECT"],["1. Joshua Lilly"],["2. Behrad Behmardi"],["3. William Zheng"],["4. Omnia Elemary"]]}))
+                         "items": [["\t\tCS 101 GROUP PROJECT"],["1. "],["2. "],["3. "],["4. "]]}))
 
     data_push_mail_notification = dict(tile='big_value',
                                        key='id_20',
                                        data=json.dumps({"title": "Mail Notification",
                                                         "description": "number of New Mails",
-                                                        "big-value": str(counter),
-                                                        "upper-left-label": "critical:",
-                                                        "upper-left-value": "1020",
-                                                        "lower-left-label": "major:",
-                                                        "lower-left-value": "8609",
-                                                        "upper-right-label": "minor:",
-                                                        "upper-right-value": "7532",
-                                                        "lower-right-label": "all:",
-                                                        "lower-right-value": "19 853"}))
+                                                        "big-value": " No New Mail",
+                                                       }))
 
 
-    data_push_mail_pie_chart = dict(tile='pie_chart', key='id_14',  data=json.dumps({"title": "Mail Analyzation", "pie_data": [["NEW MAil", 1], ["Old Mail", 18], ["Deleted", 50]]}))
+    data_push_mail_pie_chart = dict(tile='pie_chart', key='id_14',  data=json.dumps({"title": "Mail Analyzation", "pie_data": [["NEW MAil", 0], ["Old Mail", 100], ["Deleted", 0]]}))
 
     data_push_simple_percentage = dict(tile='simple_percentage',
                      key='id_15',
                  data=json.dumps({
-                         "title": "CHECKED", "subtitle": "My subtitle", "big_value": "100%",
+                         "title": "CHECKED", "subtitle": "SPEED = 72.2 mbps", "big_value": "100%",
                    "left_value": "50%", "left_label": "Old Mails",
                    "right_value": "25%", "right_label": "Unread Mails"}))
 
